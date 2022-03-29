3b18e9a77b82f1b465725aa9de4102abdaa7f2b2
grey_wolf <m15520734430@163.com>
2022-03-29 16:13:08 +0800

    base webpack config

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,32 @@
-document.body.appendChild(function(e="hellow webpack"){const n=document.createElement("div");return n.innerText=e,n}());
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("function createDom(text = 'hellow webpack') {\r\n  const div = document.createElement(\"div\")\r\n  div.innerText = text\r\n  return div\r\n}\r\ndocument.body.appendChild(createDom())\r\n\n\n//# sourceURL=webpack://webpack/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;

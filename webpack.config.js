8eb593e9c38f6350f8fff882924f7e6969b55582
grey_wolf <m15520734430@163.com>
2022-03-30 17:17:21 +0800

    comment added

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 // const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const path = require('path')
 module.exports = {
-  mode: 'development',
-  entry: {
+  mode: 'development', // 项目打包环境
+  entry: { // 配置入口
     index: './main.js',
     duplicate: './duplicate.js'  // 多入口
   },
-  devtool: 'inline-source-map', 
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
+  output: { //输出文件
+    filename: '[name].js', // 输出的文件名称
+    path: path.resolve(__dirname, 'dist'), // 输出的文件路径
+    clean: true // 输出之前 先删除原有文件
   },
-  module: {
-    rules: [
+  devtool: 'inline-source-map', // 源代码映射关系
+  devServer: {  // 配置webpack-dev-server
+    static: './dist'
+  },
+  module: { // 打包模块
+    rules: [ //打包模块使用的规则
       {
         test: /.css$/i,
         use: ['style-loader', 'css-loader']
@@ -29,7 +32,7 @@ module.exports = {
       }
     ]
   },
-  plugins: [
+  plugins: [ // 打包时候需要用到的插件
     new HtmlWebpackPlugin({
       title: 'learn webpack'
     }),

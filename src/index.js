f125727e03661297f3abf5bbb2b457b07551a529
grey_wolf <m15520734430@163.com>
2022-03-30 15:01:49 +0800

    feat: base

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import'./css/index.css'
+import head from './assets/header.png'
 function createDom(text = 'hellow webpack') {
   const div = document.createElement("div")
   div.innerText = text
   return div
 }
+const img = document.createElement(img)
+img.src = head
 document.body.appendChild(createDom())
+document.body.appendChild(img)

ae87b05aaa267a853d705171565f08f12b82a447
grey_wolf <m15520734430@163.com>
2022-04-01 15:27:04 +0800

    modified files

--- webpack.hot.js
+++ webpack.hot.js
@@ -8,11 +8,6 @@ module.exports = {
       import: './main.js',
       // dependOn: 'shared'
     },
-    // duplicate: {
-    //   import: './duplicate.js',
-    //   dependOn: 'shared'
-    // },  // 多入口
-    // shared: 'lodash', // 公共的模块  与上面 entry.XX.depenOn 对应
   },
   output: { //输出文件
     filename: '[name].js', // 输出的文件名称
@@ -20,12 +15,6 @@ module.exports = {
     // publicPath: '/', // wenpack-dev-middleware  使用的路径 引入的根路径
     clean: true, // 输出之前 先删除原有文件
     pathinfo: false, //  不输出路径关系
-  },
-  devtool: 'inline-source-map', // 源代码映射关系
-  // devtool: 'cheap-source-map',
-  // devtool: 'eval-cheap-module-source-map',
-  devServer: {  // 配置webpack-dev-server
-    static: './docs'
   },
   module: { // 打包模块
     rules: [ //打包模块使用的规则
@@ -51,12 +40,6 @@ module.exports = {
       }
     ]
   },
-  // optimization: {
-  //   runtimeChunk: 'single',
-  //   splitChunks: { //将公共的模块提取到一个新的打包文件中
-  //     chunks: 'all'
-  //   }
-  // },
   // 解析规则
   resolve: {
     extensions: ['.ts', '.js'] // 扩展 => 没有文件后缀的时候 依次在这个数组里面匹配文件<从左往右>

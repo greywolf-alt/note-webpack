a1587325441d73a9456b1e33e211ab3a69aff9cd
grey_wolf <m15520734430@163.com>
2022-04-07 11:11:08 +0800

    document.body add image

--- src/entry/assets.js
+++ src/entry/assets.js
@@ -1,1 +1,8 @@
 console.log('assets files')
+import header from '../assets/header.png'
+console.log(header)
+const image = new Image()
+image.src = header
+image.onload = () => {
+  document.body.appendChild(image)
+}
